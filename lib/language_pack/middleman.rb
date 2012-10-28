ec27acac1c0138c4a94b63745e4357e321c08e01
PJ Kelly <me@pjkel.ly>
2012-10-28 13:47:20 -0700

    Getting more specific when detecting a Middleman site.

--- lib/language_pack/middleman.rb
+++ lib/language_pack/middleman.rb
@@ -4,10 +4,11 @@ require "language_pack/rack"
 # Middleman Language Pack.
 class LanguagePack::Middleman < LanguagePack::Rack
 
-  # detects if this is a valid Middleman site by seeing if "config.rb" exists
+  # detects if this is a valid Middleman site by seeing if
+  # "source" directory and "config.rb" file both exist
   # @return [Boolean] true if it's a Middleman app
   def self.use?
-    super && File.exist?("config.rb")
+    super && Dir.exist?("source") && File.exist?("config.rb")
   end
 
   def name
