17d123c85146ec23963fe2e23e2b52faa8904cc3
PJ Kelly <me@pjkel.ly>
2012-10-28 13:24:08 -0700

    Added language pack for Middleman.

---
+++ lib/language_pack/middleman.rb
@@ -0,0 +1,30 @@
+require "language_pack"
+require "language_pack/rack"
+
+# Middleman Language Pack.
+class LanguagePack::Middleman < LanguagePack::Rack
+
+  # detects if this is a valid Middleman site by seeing if "config.rb" exists
+  # @return [Boolean] true if it's a Middleman app
+  def self.use?
+    super && File.exist?("config.rb")
+  end
+
+  def name
+    "Middleman"
+  end
+
+  def compile
+    super
+    allow_git do
+      build_middleman_site
+    end
+  end
+
+  private
+
+  def build_middleman_site
+    topic("Building Middleman Site")
+    pipe("env PATH=$PATH bundle exec middleman build --clean 2>&1")
+  end
+end
